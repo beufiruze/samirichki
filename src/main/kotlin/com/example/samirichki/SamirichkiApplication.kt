3cc2df1827c0c921b2cc27e349434d6708da714e
Firuze <fmehdizade@std.beu.edu.az>
2024-10-25 13:35:00 +0400

    tsk

---
+++ src/main/kotlin/com/example/samirichki/SamirichkiApplication.kt
@@ -0,0 +1,12 @@
+package com.example.samirichki
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SamirichkiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SamirichkiApplication>(*args)
+}
+salam samira
---
+++ src/test/kotlin/com/example/samirichki/SamirichkiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.samirichki
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SamirichkiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
